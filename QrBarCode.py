1dfb271e9be4d903250b3e84e0c6ece026a4ae0a
abdelmalek ghazy <165567049+abdelmalekghazy@users.noreply.github.com>
2024-07-28 10:55:43 +0300

    first commit

---
+++ QrBarCode.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+
+#img = cv2.imread('imageQr1.jpg')
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+with open('myDataFile.text') as f:
+    myDataList = f.read().splitlines()
+    
+   
+        
+while True:
+    succes, img = cap.read()
+    for barcode in decode(img):
+        print(barcode.data)
+        myData = barcode.data.decode('utf-8')
+        print(myData)
+        if myData in myDataList:
+            print('Authorized')
+            Color = (0,0,255)
+        else:
+            print('un-Authorized')
+            Color = (0,0,255)
+        pts = np.array([barcode.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts],True,(255,0,255),5)
+
+    cv2.imshow('Result',img)
+    cv2.waitKey(1)
+
